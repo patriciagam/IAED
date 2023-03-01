f04188e02190eea9c2077f076e1c28e78212143c
Patrícia <patricia.a.gameiro@gmail.com>
2023-03-01 18:17:56 +0000

    Update ex01.2.c

--- LAB02/ex01.2.c
+++ LAB02/ex01.2.c
@@ -2,19 +2,18 @@
 
 int main () {
     int num1, num2, num3, max;
-
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    if (num1 > num2 && num1 > num3) {
+    if (num1 > num2 && num1 > num3)
         max = num1;
-    }
-    else if (num2 > num1 && num2 > num3) {
+    
+    else if (num2 > num1 && num2 > num3) 
         max = num2;
-    }
-    else {
+    
+    else 
         max = num3;
-    }
 
     printf("%d\n", max);
-    return 0;
+    
+    return 0; 
 }
